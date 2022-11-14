e08c2e1d14c0ff6a4f77b70b0dea7f5b8a725ae9
Nathan Graule <solarliner@gmail.com>
2022-11-14 21:19:09 +0100

    wip: RPCA class in playground notebook

--- bdrpca/__init__.py
+++ bdrpca/__init__.py
@@ -10,22 +10,23 @@ from bdrpca.drpca import DRPCA
 
 _M: TypeAlias = np.ndarray[tuple[int, int], np.dtype[np.float_]]
 
-IMG_WIDTH = 1000
+IMG_WIDTH = 161
 
 
-def args() -> tuple[Path]:
+def args() -> tuple[Path, str]:
     p = ArgumentParser()
     p.add_argument('input', type=Path)
+    p.add_argument('--name', type=str, help='Name within .mat file', default='M1')
 
     args = p.parse_args()
-    return args.input,
+    return args.input, args.name
 
 
 def run():
-    path, = args()
+    path, name = args()
 
     pca = DRPCA()
-    mat: _M = io.matlab.loadmat(path)["phantom_amplitudes"].reshape((IMG_WIDTH, -1))
+    mat: _M = io.matlab.loadmat(path)[name]
     Image.fromarray(mat * 255).convert('L').save('input.png')
     T, x = pca(mat)
     Image.fromarray(T * 255).convert('L').save('output_T.png')
